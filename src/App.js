e89d0533b9b8e92dc9397c3b6bfe758865cb06b0
Philip Troy Routley <ptroutley@comcast.net>
2020-08-24 11:11:47 -0700

    updating packages

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-		  I was changed on the feature branch
+		  I was changed!!
         </p>
         <a
           className="App-link"
